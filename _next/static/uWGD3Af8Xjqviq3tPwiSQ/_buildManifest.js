bbbf8fddcf64cc6f0713f5c42b56a11b04e3547a
Fendi Rusly <fendi@Fendis-MacBook-Air.local>
2023-03-16 17:46:40 +0800

    Updates

---
+++ _next/static/uWGD3Af8Xjqviq3tPwiSQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/689-26440cacf8a187a8.js","static/chunks/pages/index-5fbdec20eee75f59.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
